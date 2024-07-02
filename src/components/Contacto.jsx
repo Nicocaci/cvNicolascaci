962a088afd07fa410fd6300d98bd9702346bf0ef
Nicolas Caci <nicko.caci@gmail.com>
2024-07-02 12:38:54 -0300

    cambios formulario

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -21,56 +21,60 @@ export default function Contacto() {
 
     return (
         <section id="contacto" className="text-white ps-5 pe-5 pt-5">
-            <h3 className="titulos">Contacto</h3>
-            <div>
-                <form onSubmit={(e) => Cargar(e)} className="row">
-                    <div class="mb-3 col-4">
+            <h3 className="titulos center ">Contacto</h3>
+            <div className="center">
+                <form onSubmit={(e) => Cargar(e)}  style={{width:"60%"}}>
+                    <div class="mb-3  ">
                         <label for="nombre" class="form-label">Nombre</label>
-                        <input type="text" onChange={(e) => setInfoContacto((prevState) => ({
+                        <input className="  form-control" type="text" onChange={(e) => setInfoContacto((prevState) => ({
                             ...prevState,
                             nombre: e.target.value
                         })
                         )} class="form-control" id="nombre" placeholder="Raul" />
                     </div>
-                    <div class="mb-3 col-3">
+                    <div class="mb-3">
                         <label for="apellido" class="form-label">Apellido</label>
-                        <input type="text" onChange={(e) => setInfoContacto((prevState) => ({
+                        <input className=" form-control" type="text" onChange={(e) => setInfoContacto((prevState) => ({
                             ...prevState,
                             apellido: e.target.value
                         })
                         )} class="form-control" id="apellido" placeholder="Rodriguez" />
                     </div>
-                    <div class="mb-3 col-3">
+                    <div class="mb-3">
                         <label for="telefono" class="form-label">Telefono</label>
-                        <input type="number" onChange={(e) => setInfoContacto((prevState) => ({
+                        <input className=" form-control" type="number" onChange={(e) => setInfoContacto((prevState) => ({
                             ...prevState,
                             telefono: e.target.value
                         })
                         )} class="form-control" id="telefono" placeholder="15-xxxx-xxxx" />
                     </div>
-                    <div class="mb-3 col-3">
+                    <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" onChange={(e) => setInfoContacto((prevState) => ({
+                        <input className="  form-control" type="email" onChange={(e) => setInfoContacto((prevState) => ({
                             ...prevState,
                             email: e.target.value
                         })
                         )} class="form-control" id="email" placeholder="name@example.com" />
                     </div>
-                    <div class="mb-3 col-9">
+                    <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Deja tu Comentario</label>
-                        <textarea onChange={(e) => setInfoContacto((prevState) => ({
+                        <textarea className=" form-control" onChange={(e) => setInfoContacto((prevState) => ({
                             ...prevState,
                             comentario: e.target.value
                         })
                         )} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
                     <div className="center">
-                        <button className='btn btn-outline-light' type="submit">Sumbit</button>
+                        <button className='btn btn-outline-light mb-5' type="submit">Sumbit</button>
                     </div>
                 </form>
-                {
+                
+            </div>
+            {
                     sumbit ?
-                        <section className="">
+                        <section className="center mb-3">
+                        <section className="center card bg-dark text-white w-25">
+                            <div className="mt-3 mb-3">
                             <div  className="d-flex me-3">
                                 <h5>Nombre: </h5>
                                 <h5 className="ms-4">{infoContacto.nombre}</h5>
@@ -87,9 +91,10 @@ export default function Contacto() {
                             <div className="d-flex me-3">              
                                 <h5>Comentario: </h5>
                                 <h5 className="ms-4">{infoContacto.comentario}</h5></div>
+                            </div>
+                        </section>
                         </section> : null
                 }
-            </div>
         </section>
 
     )
